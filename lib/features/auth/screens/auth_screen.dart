ac506e416bc949d6f44daacfcf74f5488100b8a4
SiddheshKukade <littlelord124@gmail.com>
2022-07-09 13:39:20 +0530

    Add/auth-screen-ui

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,17 +1,119 @@
+import 'package:amazon_clone_flutter/common/widgets/custom_textfield.dart';
+import 'package:amazon_clone_flutter/constants/global_varaibles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+enum Auth {
+  signin,
+  signup,
+}
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
-  static const String routeName = '/auth-screem';
+  static const String routeName = '/auth-screen';
+
   @override
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
+  final _signupFormKey = GlobalKey<FormState>();
+  final _signInFormKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: GlobalVariables.greyBackgroundCOlor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                const Text(
+                  "Welcome ! ",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                ListTile(
+                  title: const Text(
+                    "Create an accouunt",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Radio(
+                    activeColor: GlobalVariables.secondaryColor,
+                    value: Auth.signup,
+                    groupValue: _auth,
+                    onChanged: (Auth? val) {
+                      setState(() {
+                        _auth = val!;
+                      });
+                    },
+                  ),
+                ),
+                if (_auth == Auth.signup)
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    color: GlobalVariables.backgroundColor,
+                    child: Form(
+                      key: _signupFormKey,
+                      child: Column(
+                        children: [
+                          CustomTextField(
+                            hintText: "Name BRUH ! ",
+                            controller: _nameController,
+                          ),
+                          CustomTextField(
+                            hintText: "E-mail BRUH ! ",
+                            controller: _emailController,
+                          ),
+                          CustomTextField(
+                            hintText: "Password BRUH ! sshhh!! ",
+                            controller: _passwordController,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ListTile(
+                  title: const Text(
+                    "Sign In ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Radio(
+                    activeColor: GlobalVariables.secondaryColor,
+                    value: Auth.signin,
+                    groupValue: _auth,
+                    onChanged: (Auth? val) {
+                      setState(() {
+                        _auth = val!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
