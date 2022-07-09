f6d9ad71d290259b646e2e5b3c9478bc0978edd3
SiddheshKukade <littlelord124@gmail.com>
2022-07-09 17:34:45 +0530

    Add/ignore-for-node-server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+
+
+
+
+
+app.listen(4000 , "0.0.0.0",()=>console.log("port is running good 4000"))
