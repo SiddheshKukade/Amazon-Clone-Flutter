de4efd8eb77a3f2b2fe1ca8f5d84408111622a58
SiddheshKukade <littlelord124@gmail.com>
2022-07-10 18:55:38 +0530

    add/server/SingUp-SignIn

--- server/index.js
+++ server/index.js
@@ -6,15 +6,17 @@ require('dotenv').config()
 //local imports
 const authRouter = require("./routes/auth")
 const PORT = process.env.PORT || 4000;
- 
+
 //init
 const app = express();
 
- app.use(express.json())
- app.use(morgan('tiny'));
+app.use(express.json())
+app.use(morgan('tiny'));
 //middlewares
 app.use(authRouter)
-
+app.get("/", (req, res) => {
+    res.json({ "〽️ server-mood 〽️": "😀" });
+});
 mongoose.connect(process.env.MONGO_DB_URL, () => { console.log("Databse Connected " + new Date().getHours() + " : " + new Date().getMinutes(),) }).then(() => { }).catch(err => console.log("error occured " + new Date().getHours() + " : " + new Date().getMinutes(), err));
 
-app.listen(PORT, "0.0.0.0", () => console.log("port is running good "+PORT))
+app.listen(PORT, () => console.log("😀 " + PORT))
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const authRouter = express.Router();
 var User = require("./models/user")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 authRouter.post('/api/signup', async (req, res) => {
     try {
@@ -17,19 +18,38 @@ authRouter.post('/api/signup', async (req, res) => {
             return res.status(400).json({ msg: "User already exists with the E-mail !" })
         }
 
-        const hashedPassword = await bcrypt.hash(password , 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
         let user = new User({
-            email, password : hashedPassword, name
+            email, password: hashedPassword, name
         });
         user = await user.save();
         res.json(user)
     }
 
     catch (e) {
-        res.json({errMsg : e.message})
+        res.status(500).json({ errMsg: e.message })
     }
 }
-)
+);
+
+authRouter.post("/api/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ msg: "User with this E-mail Does not exits! Try Signing up " })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Password is Incorrect !  " })
+        }
+        const token = jwt.sign({ id: user._id }, "passwordKey")
+        res.json({ token, ...user._doc })
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+})
 
 
 module.exports = authRouter;
