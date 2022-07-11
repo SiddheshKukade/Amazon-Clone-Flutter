5f409ad3622132c7672ee54a965a489ea5dc74c7
SiddheshKukade <littlelord124@gmail.com>
2022-07-11 18:27:37 +0530

    Done/Profile-UI

---
+++ lib/features/account/widgets/orders.dart
@@ -0,0 +1,66 @@
+import 'package:amazon_clone_flutter/constants/global_varaibles.dart';
+import 'package:amazon_clone_flutter/features/account/widgets/single_product.dart';
+import 'package:flutter/material.dart';
+
+class Orders extends StatefulWidget {
+  const Orders({Key? key}) : super(key: key);
+
+  @override
+  State<Orders> createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  //temp list
+  List list = [
+    "https://images.unsplash.com/photo-1657482525632-dce3d15d58dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1657482525632-dce3d15d58dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1657482525632-dce3d15d58dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 15),
+              child: const Text(
+                "Your Orders",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 15),
+              child: Text(
+                "See All",
+                style: TextStyle(
+                  color: GlobalVariables.selectedNavBarColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+        //show the orders
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(
+            left: 10,
+            top: 20,
+            right: 0,
+          ),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: list.length,
+            itemBuilder: ((context, index) {
+              return SingleProduct(image: list[index]);
+            }),
+          ),
+        )
+      ],
+    );
+  }
+}
